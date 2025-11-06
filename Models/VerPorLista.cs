258d8f3bcd2ba7be528725991367cd4dc14ba2e3
Fiorella <fiorella.camayoc@gmail.com>
2025-11-06 18:07:33 -0500

    luego sigo nicol me duele la espalda

---
+++ Models/VerPorLista.cs
@@ -0,0 +1,21 @@
+
+using System.Collections.Generic;
+
+namespace PROYECTOMOVIE.Models // O el namespace que estés usando
+{
+    public class VerPorLista
+    {
+        // Esta propiedad contendrá la lista de películas
+        public List<Pelicula> Peliculas { get; set; }
+        
+        // Esta propiedad contendrá la lista de series
+        public List<Serie> Series { get; set; } // Asumo que tienes un modelo 'Serie'
+
+        // Constructor para inicializar las listas y evitar errores
+        public VerPorLista()
+        {
+            Peliculas = new List<Pelicula>();
+            Series = new List<Serie>();
+        }
+    }
+}
